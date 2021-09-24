3fb1c9bc83232959c835f34a2273850c451f38f4
Lap Ming Lee <lapminglee@protonmail.ch>
2021-09-24 14:41:19 +0800

    Fix: nil server auth code

--- packages/google_sign_in/google_sign_in/ios/Classes/FLTGoogleSignInPlugin.m
+++ packages/google_sign_in/google_sign_in/ios/Classes/FLTGoogleSignInPlugin.m
@@ -112,7 +112,7 @@ static FlutterError *getFlutterError(NSError *error) {
       result(error != nil ? getFlutterError(error) : @{
         @"idToken" : authentication.idToken,
         @"accessToken" : authentication.accessToken,
-        @"serverAuthCode" : currentUser.serverAuthCode,
+        @"serverAuthCode" : currentUser.serverAuthCode != nil ? currentUser.serverAuthCode : "",
       });
     }];
   } else if ([call.method isEqualToString:@"signOut"]) {
